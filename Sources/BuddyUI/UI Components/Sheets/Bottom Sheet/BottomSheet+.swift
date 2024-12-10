8c8bed20476e8412b6015f8473450de21fb717a1
Dmitry Novikov <nvkv.dmitry@gmail.com>
2024-12-11 02:19:30 +0300

    refactor: change bottom sheet logic & remove adaptive popup

--- Sources/BuddyUI/UI Components/Sheets/Bottom Sheet/BottomSheet+.swift
+++ Sources/BuddyUI/UI Components/Sheets/Bottom Sheet/BottomSheet+.swift
@@ -2,7 +2,7 @@
 //  BottomSheet+.swift
 //  BuddyUI
 //
-//  Created by Dimka Novikov on 09.12.2024.
+//  Created by Dimka Novikov on 11.12.2024.
 //  Copyright © 2024 DDEC. All rights reserved.
 //
 
@@ -17,40 +17,14 @@ import SwiftUI
 
 @available(iOS 18.0, *)
 extension View {
-
-    // MARK: - Public methods
-
-    ///
-    ///
-    ///
-    public func bottomSheet<Content: View>(
+    public func bottomSheet2<Content: View>(
         isPresented: Binding<Bool>,
-        @ViewBuilder content: @escaping () -> Content
+        @ViewBuilder content: () -> Content
     ) -> some View {
-        self.onChange(of: isPresented.wrappedValue) {
-            guard isPresented.wrappedValue else { return }
-
-            let window = UIApplication.shared.connectedScenes
-                .compactMap { $0 as? UIWindowScene }
-                .flatMap { $0.windows }
-                .first(where: { $0.isKeyWindow })
-
-            if let window {
-                let bottomSheet = BottomSheet(isPresented: isPresented, content: content) {
-                    window.rootViewController?.children.last?.view.removeFromSuperview()
-                    window.rootViewController?.children.last?.removeFromParent()
-                }
-
-                let viewController = UIHostingController(rootView: bottomSheet)
-
-                viewController.view.frame = window.rootViewController?.view.bounds ?? .zero
-                viewController.view.backgroundColor = .clear
-                viewController.view.translatesAutoresizingMaskIntoConstraints = false
-
-                window.rootViewController?.view.addSubview(viewController.view)
-                window.rootViewController?.addChild(viewController)
+        self.overlay {
+            if isPresented.wrappedValue {
+                BottomSheet(isPresented: isPresented, content: content)
             }
         }
     }
 }
-
--- Sources/BuddyUI/UI Components/Sheets/Bottom Sheet/BottomSheet.swift
+++ Sources/BuddyUI/UI Components/Sheets/Bottom Sheet/BottomSheet.swift
@@ -2,7 +2,7 @@
 //  BottomSheet.swift
 //  BuddyUI
 //
-//  Created by Dimka Novikov on 09.12.2024.
+//  Created by Dimka Novikov on 11.12.2024.
 //  Copyright © 2024 DDEC. All rights reserved.
 //
 
@@ -18,46 +18,46 @@ import SwiftUI
 @available(iOS 18.0, *)
 struct BottomSheet<Content: View>: View {
 
-    // MARK: - Public typealiases
-
-    typealias Action = () -> Void
-
-
-
     // MARK: - Private properties
 
     @Binding private var isPresented: Bool
     private let content: Content
-    private let action: Action
 
     @State private var isShowed = false
-    @State private var yOffset: CGFloat = .zero
+    @State private var offset: CGFloat = .zero
 
 
 
     // MARK: - Body
 
     var body: some View {
-        Color.black.opacity(0.6)
-            .opacity(isShowed ? 1 : .zero)
-        // FIXME: This event causes removing key window
-//            .onTapGesture { hide() }
+        BlurView(radius: isShowed ? 5 : .zero)
+            .overlay {
+                Color.black.opacity(0.5)
+                    .opacity(isShowed ? 1 : .zero)
+            }
+            .onTapGesture {
+                withAnimation(.interactiveSpring(duration: 0.3)) {
+                    isShowed = false
+                } completion: {
+                    isPresented = isShowed
+                }
+            }
             .overlay(alignment: .bottom) {
                 if isShowed {
                     content
-                        .offset(y: yOffset)
-                    // FIXME: This event causes removing key window
-//                        .gesture(
-//                            DragGesture()
-//                                .onChanged { gesture in
-//                                    if gesture.startLocation.y < gesture.location.y {
-//                                        yOffset = gesture.translation.height
-//                                    }
-//                                }
-//                                .onEnded { gesture in
-//                                    if gesture.translation.height > 100 { hide() }
-//                                }
-//                        )
+                        .offset(y: offset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.startLocation.y < gesture.location.y {
+                                        offset = gesture.translation.height
+                                    }
+                                }
+                                .onEnded { gesture in
+                                    if gesture.translation.height > 50 { hide() } else { resetOffset() }
+                                }
+                        )
                         .transition(
                             .asymmetric(
                                 insertion: .move(edge: .bottom),
@@ -79,12 +79,10 @@ struct BottomSheet<Content: View>: View {
 
     init(
         isPresented: Binding<Bool>,
-        content: () -> Content,
-        action: @escaping Action
+        content: () -> Content
     ) {
         _isPresented = isPresented
         self.content = content()
-        self.action = action
     }
 
 
@@ -96,8 +94,6 @@ struct BottomSheet<Content: View>: View {
             self.isShowed = isShowed
         } completion: {
             isPresented = isShowed
-
-            action()
         }
     }
 
@@ -106,84 +102,10 @@ struct BottomSheet<Content: View>: View {
             isShowed = isPresented
         }
     }
-}
 
-
-
-// MARK: - Live Preview
-
-#Preview {
-    @Previewable @State var selectedIndex: Int = .zero
-    @Previewable @State var isBottomSheetPresented = false
-
-    let count = 10
-
-    VStack(spacing: .zero) {
-        HStack {
-            Text("Selected Index = \(selectedIndex)")
-
-            Spacer()
-
-            Button("Open Bottom Sheet") {
-                isBottomSheetPresented = true
-            }
-            .buttonStyle(.borderedProminent)
-        }
-        .padding()
-        .background(.yellow)
-
-        ScrollView(.vertical) {
-            LazyVStack(spacing: .zero) {
-                ForEach(.zero ..< 100, id: \.self) { index in
-                    Text("Item = \(index)")
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical)
-                        .background(.black.opacity(0.3))
-                        .clipShape(.rect(cornerRadius: 10))
-                        .padding(.vertical, 10)
-                        .padding(.horizontal)
-                }
-            }
-        }
-    }
-    .bottomSheet(isPresented: $isBottomSheetPresented) {
-        GeometryReader { geometry in
-            let size = geometry.size
-
-            VStack(spacing: .zero) {
-                Text("Title")
-                    .frame(maxWidth: .infinity)
-                    .overlay(alignment: .trailing) {
-                        Button {
-                            isBottomSheetPresented = false
-                        } label: {
-                            Image(systemName: "xmark")
-                        }
-                        .buttonStyle(.plain)
-                    }
-                    .foregroundStyle(.black.opacity(0.8))
-                    .padding()
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(.red, lineWidth: 1)
-                    }
-
-                List {
-                    ForEach(.zero ..< count, id: \.self) { index in
-                        Button {
-                            selectedIndex = index
-                            isBottomSheetPresented = false
-                        } label: {
-                            Text("Item = \(index)")
-                        }
-                    }
-                }
-                .frame(height: count > 6 ? (size.height / 2) : CGFloat(count) * 40 + 100)
-            }
-            .padding(.bottom, 30)
-            .background(.white)
-            .clipShape(.rect(cornerRadius: 20))
-            .frame(width: size.width, height: size.height, alignment: .bottom)
+    private func resetOffset() {
+        withAnimation(.interactiveSpring(duration: 0.4)) {
+            offset = .zero
         }
     }
 }
